02b2f80c5e95bf2903903504df1fc57ec9e5f2f1
Cristian Cervantes <cristoichkov@gmail.com>
2020-02-25 22:11:46 -0600

    function R

---
+++ bin/filter_genes.R
@@ -0,0 +1,25 @@
+## this function filters genes that are not present in the selected species (delete zeros)##
+
+## Defines the name of the function, which accepts as parameters: 
+## file = "../data.txt", species = c("ath", "ach", "dca", "ccan")
+
+filter_genes <- function(file, species){
+  
+  SCG_plaza <- read.delim(file) ## load the base obtained from PLAZA
+  
+  for (i in 1:length(colnames(SCG_plaza))){ ## list the columns
+    
+    for (k in 1:length(species)){ ## list the species that were selected
+      
+      if(colnames(SCG_plaza)[i] == species[k]) {  ## if the species is equal to the name of the column then the following is done
+        
+        SCG_plaza <- subset(SCG_plaza, SCG_plaza[,i] != 0) ##  delete genes that are not present in the species we select
+        
+      }
+    }
+  }
+  
+  write.csv(SCG_plaza, file = "../out/filter_genes.csv") ## save the table with the filtered genes
+}
+
+
